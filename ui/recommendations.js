6f4413e9e4bd8bc25f3bd921a1e3745d3255e4cf
kptdobe <acapt@adobe.com>
2025-03-27 10:14:16 +0100

    chore: do not break if no time

--- ui/recommendations.js
+++ ui/recommendations.js
@@ -4,7 +4,7 @@
         div.className = `recommendation ${rec.passing ? 'passing' : 'failed'}`;
         
         let html = `
-            <div class="recommendation-time">${rec.time.toFixed(0)}ms</div>
+            <div class="recommendation-time">${rec.time ? rec.time.toFixed(0) + 'ms' : ''}</div>
             <div class="recommendation-category">${rec.category.toUpperCase()}</div>
             <div class="recommendation-message">${rec.message}</div>
             ${rec.recommendation ? `<div class="recommendation-text"><p>${rec.recommendation}</p></div>` : ''}
