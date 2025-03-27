88e6c59cd0002b1041c90be406fe410e7f86d934
Julien Ramboz <ramboz@adobe.com>
2025-03-27 14:50:31 +0100

    fix: fonts rule

--- src/rules/fonts.js
+++ src/rules/fonts.js
@@ -1,64 +1,57 @@
-export default function evaluate({ har, perfEntries, jsApi }) {
+export default function evaluate({ har, jsApi }) {
   const allFonts = har.log.entries.filter((e) => e.response.content.mimeType.includes('font'));
   if (!allFonts.length) {
     return null;
   }
-  const allModernFonts = allFonts.every((e) => e.response.content.mimeType.includes('woff2'));
-  if (!allModernFonts) {
-    return {
-      category: 'lcp',
-      message: 'Use a modern font format for custom fonts',
-      recommendation: 'Make sure to use custom fonts that are in the WOFF2 format.',
-      passing: false,
-    };
-  }
 
-  const lcp = perfEntries.find((e) => e.entryType === 'largest-contentful-paint');
-  if (!lcp || !lcp.url) {
-    return null;
-  }
-  const lcpIndex = har.log.entries.findIndex((e) => e.request.url === lcp.url);  
-  const fontIndex = har.log.entries.findIndex((e) => e.response.content.mimeType.includes('font'));
-  if (lcpIndex > -1 && fontIndex > -1 && lcpIndex > fontIndex) {
-    return {
-      category: 'lcp',
-      message: 'Load custom fonts lazily.',
-      recommendation: 'Make sure to load custom fonts after the LCP element.',
-      passing: false,
-    };
-  }
+  // Check woff2 font format
+  const results = [];
+  allFonts.forEach((e) => {
+    if (!e.response.content.mimeType.includes('woff2')) {
+      results.push({
+        category: 'lcp',
+        message: `Use a modern font format for ${e.request.url}`,
+        recommendation: 'Make sure to use custom fonts that are in the WOFF2 format.',
+        passing: false,
+      })
+    }
+  });  
 
+  // Check "font-display: swap"
   const loadedFonts = jsApi.fonts.filter((f) => f.status === 'loaded');
-  const allFontsUseSwap = loadedFonts.every((f) => f.display === 'swap');
-  if (!allFontsUseSwap) {
-    return {
-      category: 'lcp',
-      message: 'Gracefully swap custom fonts when they are loaded.',
-      recommendation: 'Make sure to use the swap display property for custom fonts.',
-      passing: false,
-    };
-  }
+  loadedFonts.forEach((f) => {
+    if (f.display !== 'swap') {
+      results.push({
+        category: 'lcp',
+        message: `Gracefully swap in ${f.family} when the font is loaded`,
+        recommendation: 'Make sure to use the swap display property for custom fonts.',
+        passing: false,
+      });
+    }
+  });
 
   // Check that fallback fonts are used
-  const fallbackFonts = jsApi.fonts.filter((f) => f.status === 'unloaded');
+  const fallbackFonts = jsApi.fonts.filter((f) => f.family.includes('fallback'));
   if (loadedFonts.length > 0 && !fallbackFonts.length) {
-    return {
+    results.push({
       category: 'lcp',
       message: 'Use fallback fonts for all your custom fonts.',
       recommendation: 'Make sure to use configure fallback fonts to be shown while your custom fonts load.',
       passing: false,
-    };
+    });
   }
 
   // Check that fallback fonts are size adjusted
-  const allFallbackFontsSizeAdjusted = fallbackFonts.every((f) => f.sizeAdjust !== '100%');
-  if (!allFallbackFontsSizeAdjusted) {
-    return {
-      category: 'lcp',
-      message: 'Size fallback fonts to mimic custom fonts.',
-      recommendation: 'Make sure to use the swap display property for custom fonts.',
-      passing: false,
-    };
-  }
-  return null;
+  fallbackFonts.forEach((f) => {
+    if (f.sizeAdjust === '100%') {
+      results.push({
+        category: 'lcp',
+        message: 'Size fallback fonts to mimic custom fonts.',
+        recommendation: 'Make sure to use the swap display property for custom fonts.',
+        passing: false,
+      });
+    }
+  });
+
+  return results;
 }
