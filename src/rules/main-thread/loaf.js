05057246393517b05cc93f6f36d20d57baf9ab19
kptdobe <acapt@adobe.com>
2025-04-03 15:45:59 +0200

    feat: remove loaf generated by blocking css

--- src/rules/main-thread/loaf.js
+++ src/rules/main-thread/loaf.js
@@ -18,15 +18,24 @@ export default function evaluate({ report }) {
         };
       } else {
         // find scripts and styles that are blocking the main thread during the animation frame
-        const blockingResources = data.filter(e => e.entryType === 'resource' && e.start >= start && e.end <= end && (e.initiatorType === 'script' || e.initiatorType === 'link'));
-        return {
-          category: 'main-thread',
+        const blockingResources = data.filter(e => 
+          e.entryType === 'resource' &&
+          e.start >= start && e.end <= end &&
+          (e.initiatorType === 'script' || e.initiatorType === 'link')
+        );
+        // a blocking css is not a problem: it might generate an long animation frame
+        // but all solutions tested to get rid of the corresponding long animation frame do not change the timing.
+        const hasCSS = blockingResources.some(e => e.mimeType.includes('text/css'));
+        if (!hasCSS) {
+          return {
+            category: 'main-thread',
           message: `Blocking resource${blockingResources.length > 1 ? 's' : ''}`,
           url: blockingResources.map(e => e.url).join(', '),
           recommendation: `Review potential blocking resource${blockingResources.length > 1 ? 's' : ''} found during the animation frame`,
           passing: false,
           time: start,
-        };
+          };
+        }
       }
     });
   }
